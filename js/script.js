af03b401c00bd76b4e75cc5f9d34a8f5fbfe104f
HikaruFN <francesconisi95@gmail.com>
2021-04-23 18:56:06 +0200

    fine milestone 4

--- js/script.js
+++ js/script.js
@@ -135,5 +135,4 @@ var app = new Vue(
         }
     }
 );
-// Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite
-// (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
+//Visualizzazione ora e ultimo messaggio inviato/ricevuto
