54b9c5e00e1c77a9e069b8ca20fb93b24b635008
HikaruFN <francesconisi95@gmail.com>
2021-04-23 14:48:15 +0200

    fine milestone 2

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ var app = new Vue(
         el: '#root',
         data: {
             
-            contattoAttivo: 0,
+            contattoAttivo: 2,
             contacts: [
                 {
                     name: 'Michele',
@@ -13,17 +13,20 @@ var app = new Vue(
                         {
                             date: '10/01/2020 15:30:55',
                             text: 'Hai portato a spasso il cane?',
-                            status: 'sent'
+                            status: 'sent',
+                            stileMessaggio: 'message-sent'
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             text: 'Ricordati di dargli da mangiare',
-                            status: 'sent'
+                            status: 'sent',
+                            stileMessaggio: 'message-sent'
                         },
                         {
                             date: '10/01/2020 16:15:22',
                             text: 'Tutto fatto!',
-                            status: 'received'
+                            status: 'received',
+                            stileMessaggio: 'message-received'
                         }
                     ],
                 },
@@ -35,17 +38,20 @@ var app = new Vue(
                         {
                             date: '20/03/2020 16:30:00',
                             text: 'Ciao come stai?',
-                            status: 'sent'
+                            status: 'sent',
+                            stileMessaggio: 'message-sent'
                         },
                         {
                             date: '20/03/2020 16:30:55',
                             text: 'Bene grazie! Stasera ci vediamo?',
-                            status: 'received'
+                            status: 'received',
+                            stileMessaggio: 'message-received'
                         },
                         {
                             date: '20/03/2020 16:35:00',
                             text: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                            status: 'sent'
+                            status: 'sent',
+                            stileMessaggio: 'message-sent'
                         }
                     ],
                 },
@@ -57,17 +63,20 @@ var app = new Vue(
                         {
                             date: '28/03/2020 10:10:40',
                             text: 'La Marianna va in campagna',
-                            status: 'received'
+                            status: 'received',
+                            stileMessaggio: 'message-received'
                         },
                         {
                             date: '28/03/2020 10:20:10',
                             text: 'Sicuro di non aver sbagliato chat?',
-                            status: 'sent'
+                            status: 'sent',
+                            stileMessaggio: 'message-sent'
                         },
                         {
                             date: '28/03/2020 16:15:22',
                             text: 'Ah scusa!',
-                            status: 'received'
+                            status: 'received',
+                            stileMessaggio: 'message-received'
                         }
                     ],
                 },
@@ -79,12 +88,14 @@ var app = new Vue(
                         {
                             date: '10/01/2020 15:30:55',
                             text: 'Lo sai che ha aperto una nuova pizzeria?',
-                            status: 'sent'
+                            status: 'sent',
+                            stileMessaggio: 'message-sent'
                         },
                         {
                             date: '10/01/2020 15:50:00',
                             text: 'Si, ma preferirei andare al cinema',
-                            status: 'received'
+                            status: 'received',
+                            stileMessaggio: 'message-received'
                         }
                     ],
                 },
@@ -92,6 +103,9 @@ var app = new Vue(
             
         },
         methods: {
+            cambiaContatto(index){
+                this.contattoAttivo = index;
+            }
             
         },
     }
